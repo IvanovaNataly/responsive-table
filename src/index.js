e87ff21ab4a694f7ecc03cc292be26123eab3e2e
IvanovaNataly <letter4natalia@gmail.com>
2017-09-06 13:26:39 +0300

    version for integration into Holdings project

--- src/index.js
+++ src/index.js
@@ -1,41 +1,67 @@
-//let $ = require("jquery");
 
-//require("../css/styles.scss");
+const cl = console.log;
 
 class ResponsiveTable {
     getData(){
-        fetch("./data/all-devices.json")
-            .then( (response) => {if (response.ok) return response.json()
-                                    else throw new Error ("No data!")
-                                })
+        $.get("./data/all-devices.json")
             .then( (data) => this.renderRows(data) )
+    }
+
+    createTableScrolls() {
+        let tableTopHeader = document.getElementById('tableHead');
+        let rightColumn = document.getElementById('rightColumn');
+        let tableBody = document.getElementById('tableBody');
+
+		tableBody.addEventListener('scroll', function (e) {
+			tableTopHeader.scrollLeft = e.target.scrollLeft;
+        }, true);
+
+		tableBody.addEventListener('scroll', function (e) {
+			$("#rightColumn").scrollTop(e.target.scrollTop);
+		}, true);
     }
 
     renderRows(data) {
         console.log(data);
-        let table = $(`<ul class="table-ul"></ul>`);
+        let tableBodyCont = $(`<div class="table-body-container"></div>`);
+
+		let rightColumn = $(`<ul class="right-column" id="rightColumn"></ul>`);
+		let rightRows = data.map( device => `
+            <li class="row">
+                <div class="right-column-div">${device.type} 
+                </div>
+            </li>` );
+        rightColumn.prepend(rightRows);
+
+
+        let table = $(`<ul class="table-body" id="tableBody" ></ul>`);
         let tableRows = data.map( device => `
             <li class="row">
-                <div>${device.amount}
-                </div><div>${device.moreInfo.mgmt}
+                </div><div>${device.moreInfo.prop}
                 </div><div>${device.moreInfo.reg}
                 </div><div>${device.subtype}
-                </div><div>${device.type}</div>
-            </li>` )
+                </div><div>${device.amount}</div>
+            </li>` );
         table.prepend(tableRows);
-        this.element.append(table);
+
+		tableBodyCont.append(rightColumn);
+		tableBodyCont.append(table);
+
+        this.element.append(tableBodyCont);
+        this.createTableScrolls();
     }
 
     render() {
         this.element = $(`<div class="table-container"></div>`);
-        let titles = $(`<ul class="table-head"><li class="head-row">
-                <div>Name
-                </div><div>Name
-                </div><div>Name
-                </div><div>Title
-                </div><div>Title</div>
-            </li>
-            </ul>` )
+        let titles = $(`<div class="table-head-container"><ul class="table-head" id="tableHead">
+                <li class="head-row">
+                    <div>Name
+                    </div><div>Name
+                    </div><div>Name
+                    </div><div>Title
+                    </div>
+                </li>
+            </ul></div>` );
         this.element.append(titles);
         this.getData();
         return this.element;
@@ -49,11 +75,5 @@ function createTable() {
 
 createTable();
 
-// (function($){
-//      $(window).on("load",function(){
-//          $(".table-ul").mCustomScrollbar({
-//              axis:"yx" // vertical and horizontal scrollbar
-//          });
-//      });
-// })(jQuery);
+
 
